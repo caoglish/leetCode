258c4c19fdb61c3bda75215ce9cde1105a7f2b7c
caoglish <caoglish@sina.com>
2018-07-04 21:25:57 +1000

    add toGoatLatin

---
+++ solution/toGoatLatin.js
@@ -0,0 +1,22 @@
+var toGoatLatin = function (S) {
+    const vowel = ["a", "e", "i", "o", "u"];
+    let slist = S.split(" ")
+
+    return slist.map(function (word, i) {
+        let letters = word.split("");
+        let first = letters[0];
+        let aWords = "ma" + "a".repeat(i + 1);
+
+        if (vowel.indexOf(first.toLowerCase()) >= 0) {
+            return word + aWords;
+        } else {
+            letters.push(first)
+            letters.shift()
+            return (letters.join("")) + aWords;
+        }
+    }).join(" ");
+
+
+}
+
+module.exports = toGoatLatin;
---
+++ test/toGoatLatin.test.js
@@ -0,0 +1,7 @@
+const toGoatLatin = require("../solution/toGoatLatin");
+
+test('toGoatLatin', () => {
+    expect(toGoatLatin("I speak Goat Latin")).toBe("Imaa peaksmaaa oatGmaaaa atinLmaaaaa");
+    expect(toGoatLatin("The quick brown fox jumped over the lazy dog")).toBe("heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa");
+    expect(toGoatLatin("Each word consists of lowercase and uppercase letters only")).toBe("Eachmaa ordwmaaa onsistscmaaaa ofmaaaaa owercaselmaaaaaa andmaaaaaaa uppercasemaaaaaaaa etterslmaaaaaaaaa onlymaaaaaaaaaa");
+});
