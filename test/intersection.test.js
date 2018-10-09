9bc977aa54bcfb1df7398efeabc2ded9ce4c75a2
caoglish <caoglish.sina@gmail.com>
2018-10-09 11:36:45 +1100

    1. add smallestRangeI

--- test/intersection.test.js
+++ test/intersection.test.js
@@ -1,7 +1,7 @@
 const intersection = require('../solution/intersection');
 const oneOfArray = require('../helper/helper').oneOfArray;
 
-test('arrayPairSum', () => {
+test('intersection', () => {
 	expect(intersection([1, 2, 2, 1], [2, 2])).toEqual([2]);
 	expect(oneOfArray(intersection([4, 9, 5], [9, 4, 9, 8, 4]), [
 		[9, 4],
